b58ad09b34793af0a3d4a927d7d1f51d2f7e0b11
Daniel Hershcovich <daniel.hershcovich@gmail.com>
2018-07-08 19:43:20 +0300

    Script to validate sentences after splitting

---
+++ scripts/validate_sentences.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+[ $BRANCH == HEAD ] && BRANCH=$TRAVIS_BRANCH
+[ $TRAVIS_PULL_REQUEST == false ] || BRANCH=pr-$TRAVIS_PULL_REQUEST
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch origin ${BRANCH}-sentences
+git checkout -b ${BRANCH}-sentences origin/${BRANCH}-sentences
+
+python -m scripts.validate *.pickle || echo Ignoring validation failures
+
